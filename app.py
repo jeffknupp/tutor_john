3144af135ce72cc3df55ca49bc9340155c0da362
Jeff Knupp <jknupp@appnexus.com>
2014-04-28 22:43:59 -0400

    Working airport load function

--- app.py
+++ app.py
@@ -2,9 +2,18 @@
 
 import sys
 
-def load_airport_info():
+AIRPORT_LOCATIONS = {}
+AIRPORT_CODES = {}
+
+def load_airport_info(airport_file):
     """Load latitude, longitude, airport code, and airport name from file."""
-    pass
+    with open(airport_file, 'r') as file_handle:
+        for line in file_handle:
+            line = line.split(',')
+            name, code, latitude, longitude = line[1].strip('"'), line[4].strip('"'), float(line[6]), float(line[7])
+            AIRPORT_LOCATIONS[code] = (latitude, longitude)
+            AIRPORT_CODES[code] = '{name} ({code})'.format(name=name, code=code)
+            AIRPORT_CODES[name] = '{name} ({code})'.format(name=name, code=code)
 
 def get_completions():
     """Return possible autocompletions for a given snippet of text."""
@@ -23,4 +32,8 @@ def main():
     pass
 
 if __name__ == '__main__':
+    load_airport_info('us_airports.txt')
+    print AIRPORT_CODES['EWR']
+    print AIRPORT_LOCATIONS['EWR']
+    print AIRPORT_CODES['Newark Liberty Intl']
     sys.exit(main())
