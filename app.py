313beac11fdb417b2c81d6dbcb8c895b9a24439c
Jeff Knupp <jknupp@appnexus.com>
2014-04-28 22:31:50 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+"""Web application that calculates the distance between two airports."""
+
+import sys
+
+def load_airport_info():
+    """Load latitude, longitude, airport code, and airport name from file."""
+    pass
+
+def get_completions():
+    """Return possible autocompletions for a given snippet of text."""
+    pass
+
+def display_form():
+    """Display airport form."""
+    pass
+
+def display_results():
+    """Show results on map."""
+    pass
+
+def main():
+    """Main entry point for program."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
